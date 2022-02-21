db10e238dfff27b31d7d3714c416b97b3f9a2b92
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-02-21 14:09:07 +0200

    DZ3.11

--- src/components/Render/Render.jsx
+++ src/components/Render/Render.jsx
@@ -8,13 +8,34 @@ import s from './Render.module.css';
 class Render extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+componentDidMount() {
+
+    const contacts = localStorage.getItem('contacts');
+    const parsedId = JSON.parse(contacts);
+
+    if (parsedId) {
+      this.setState({ contacts: parsedId });
+    }
+  }
+
+   componentDidUpdate(prevProps, prevState) {
+
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
+
   handleChange = event => {
     this.setState({ name: event.target.value });
   };
